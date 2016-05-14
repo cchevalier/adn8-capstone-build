9f21e40c6f62e0549eb2f12060a37d142eac3c69
CChevalier <cchevalier@users.noreply.github.com>
2016-05-14 14:38:38 +0200

    Implement data models as Parcelable
    using plugin:  https://github.com/mcharmas/android-parcelable-intellij-plugin

--- app/src/main/java/net/cchevalier/photosplash/models/Photo.java
+++ app/src/main/java/net/cchevalier/photosplash/models/Photo.java
@@ -1,11 +1,14 @@
 package net.cchevalier.photosplash.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
 /**
  * Created by cch on 11/05/2016.
  */
-public class Photo {
+public class Photo implements Parcelable {
 
     public final String id;
     public final int width;
@@ -47,6 +50,54 @@ public class Photo {
         res = res + "\n w: " + this.width;
         res = res + "\n h: " + this.height;
         res = res + "\n c: " + this.color;
+        res = res + "\n L: " + this.likes;
+        res = res + "\n y: " + this.liked_by_user;
+
+        for (int i = 0; i < categories.size(); i++) {
+            res = res + "\n   cat: " + this.categories.get(i).title;
+        }
         return res;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.id);
+        dest.writeInt(this.width);
+        dest.writeInt(this.height);
+        dest.writeString(this.color);
+        dest.writeInt(this.likes);
+        dest.writeByte(this.liked_by_user ? (byte) 1 : (byte) 0);
+        dest.writeParcelable(this.user, flags);
+        dest.writeTypedList(this.categories);
+        dest.writeParcelable(this.urls, flags);
+    }
+
+    protected Photo(Parcel in) {
+        this.id = in.readString();
+        this.width = in.readInt();
+        this.height = in.readInt();
+        this.color = in.readString();
+        this.likes = in.readInt();
+        this.liked_by_user = in.readByte() != 0;
+        this.user = in.readParcelable(User.class.getClassLoader());
+        this.categories = in.createTypedArrayList(Category.CREATOR);
+        this.urls = in.readParcelable(Urls.class.getClassLoader());
+    }
+
+    public static final Parcelable.Creator<Photo> CREATOR = new Parcelable.Creator<Photo>() {
+        @Override
+        public Photo createFromParcel(Parcel source) {
+            return new Photo(source);
+        }
+
+        @Override
+        public Photo[] newArray(int size) {
+            return new Photo[size];
+        }
+    };
 }
--- app/src/main/java/net/cchevalier/photosplash/models/User.java
+++ app/src/main/java/net/cchevalier/photosplash/models/User.java
@@ -1,9 +1,12 @@
 package net.cchevalier.photosplash.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by cch on 11/05/2016.
  */
-public class User {
+public class User implements Parcelable {
     public final String id;
     public final String username;
     public final String name;
@@ -13,4 +16,34 @@ public class User {
         this.username = username;
         this.name = name;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.id);
+        dest.writeString(this.username);
+        dest.writeString(this.name);
+    }
+
+    protected User(Parcel in) {
+        this.id = in.readString();
+        this.username = in.readString();
+        this.name = in.readString();
+    }
+
+    public static final Parcelable.Creator<User> CREATOR = new Parcelable.Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel source) {
+            return new User(source);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
 }
