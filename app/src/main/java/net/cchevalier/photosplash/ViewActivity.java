71275196ec98adff4232f25bb7fa95611fdadb02
CChevalier <cchevalier@users.noreply.github.com>
2016-05-23 18:43:17 +0200

    Implement correct handling of favorites in View activity

--- app/src/main/java/net/cchevalier/photosplash/ViewActivity.java
+++ app/src/main/java/net/cchevalier/photosplash/ViewActivity.java
@@ -31,30 +31,28 @@ public class ViewActivity extends AppCompatActivity {
     private Photo currentPhoto;
     private boolean isFavorite;
 
+    private FloatingActionButton fabFavorite;
     private BottomSheetBehavior mBottomSheetBehavior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_view);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-//        ActionBar actionBar = getSupportActionBar();
         getSupportActionBar().setTitle("");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // PhotoView handling
-        PhotoView photoView  = (PhotoView) findViewById(R.id.iv_photo);
-
-        final PhotoViewAttacher attacher = new PhotoViewAttacher(photoView);
-
-//        String url = getIntent().getStringExtra("url.full");
+        // Incoming intent
         currentPhoto = getIntent().getParcelableExtra("currentPhoto");
+        isFavorite = isFavorite(currentPhoto);
         Log.d(TAG, "onCreate: " + currentPhoto.toString());
 
-        isFavorite = isFavorite(currentPhoto);
-
+        // PhotoView handling
+        PhotoView photoView  = (PhotoView) findViewById(R.id.iv_photo);
+        final PhotoViewAttacher attacher = new PhotoViewAttacher(photoView);
         Picasso.with(this)
                 .load(currentPhoto.urls.regular)
                 .into(photoView, new Callback() {
@@ -68,21 +66,23 @@ public class ViewActivity extends AppCompatActivity {
                     }
                 });
 
-        // Bottom Sheet
-        TextView photoInfo = (TextView) findViewById(R.id.tv_photo_info);
-        photoInfo.setText(currentPhoto.toString());
-
-        View bottomSheet = findViewById( R.id.bottom_sheet );
-        mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
-//        mBottomSheetBehavior.setPeekHeight(100);
-        mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
-
 
         // Favorite FAB
-        FloatingActionButton fabFavorite = (FloatingActionButton) findViewById(R.id.fab_favorite);
+        fabFavorite = (FloatingActionButton) findViewById(R.id.fab_favorite);
+        updateFavoriteFAB(isFavorite);
+/*
+        if (isFavorite) {
+            fabFavorite.setImageResource(R.drawable.heart_full);
+        }
+*/
         fabFavorite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                UpdateFavoriteStatus task = new UpdateFavoriteStatus();
+                task.execute();
+
+/*
                 Snackbar.make(view, "Added to your favorites", Snackbar.LENGTH_LONG)
                         .setAction("Undo", new View.OnClickListener() {
                             @Override
@@ -105,18 +105,27 @@ public class ViewActivity extends AppCompatActivity {
                             .show();
 
                     isFavorite = !isFavorite;
-
                 } else {
-
                     Snackbar.make(view, "Already in your favorites", Snackbar.LENGTH_LONG)
                             .setAction("Undo", null)
                             .show();
-
                 }
+*/
 
             }
         });
 
+        // Info
+        //
+        // Sample Info text
+        TextView photoInfo = (TextView) findViewById(R.id.tv_photo_info);
+        photoInfo.setText(currentPhoto.toString());
+        //
+        // Bottom Sheet
+        View bottomSheet = findViewById( R.id.bottom_sheet );
+        mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        //
         // Info FAB
         FloatingActionButton fabInfo = (FloatingActionButton) findViewById(R.id.fab_info);
         fabInfo.setOnClickListener(new View.OnClickListener() {
@@ -129,17 +138,19 @@ public class ViewActivity extends AppCompatActivity {
                 }
             }
         });
-
     }
 
+/*
 
     private void addToFavorites() {
 
         new AsyncTask<Void, Void, Void>() {
 
-            /**
+            */
+/**
              * Override this method to perform a computation on a background thread.
-             */
+             *//*
+
             @Override
             protected Void doInBackground(Void... params) {
 
@@ -156,10 +167,75 @@ public class ViewActivity extends AppCompatActivity {
                 values.put(PhotosplashContract.Favorites.COLUMN_URLS_SMALL, currentPhoto.urls.small);
 
                 Uri uri = getContentResolver().insert(PhotosplashContract.Favorites.CONTENT_URI, values);
-                Log.d(TAG, "doInBackground: " + uri.toString());
+//                Log.d(TAG, "doInBackground: " + uri.toString());
                 return null;
             }
         }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+    }
+
+*/
+
+    private class UpdateFavoriteStatus extends AsyncTask<Void, Void, Boolean> {
+
+        /**
+         * Override this method to perform a computation on a background thread.
+         */
+        @Override
+        protected Boolean doInBackground(Void... params) {
+
+            Boolean currentStatus = isFavorite;
+            Boolean newStatus = currentStatus;
+
+            if (currentStatus) {
+
+                getContentResolver().delete(
+                        PhotosplashContract.Favorites.CONTENT_URI,
+                        PhotosplashContract.Favorites.COLUMN_PHOTO_ID + " = '" + currentPhoto.id + "'",
+                        null
+                );
+
+                newStatus = false;
+            } else {
+                ContentValues values = new ContentValues();
+                values.put(PhotosplashContract.Favorites.COLUMN_PHOTO_ID, currentPhoto.id);
+                values.put(PhotosplashContract.Favorites.COLUMN_WIDTH, currentPhoto.width);
+                values.put(PhotosplashContract.Favorites.COLUMN_HEIGHT, currentPhoto.height);
+                values.put(PhotosplashContract.Favorites.COLUMN_COLOR, currentPhoto.color);
+                values.put(PhotosplashContract.Favorites.COLUMN_USER_ID, currentPhoto.user.id);
+                values.put(PhotosplashContract.Favorites.COLUMN_USER_NAME, currentPhoto.user.name);
+                values.put(PhotosplashContract.Favorites.COLUMN_URLS_FULL, currentPhoto.urls.full);
+                values.put(PhotosplashContract.Favorites.COLUMN_URLS_REGULAR, currentPhoto.urls.regular);
+                values.put(PhotosplashContract.Favorites.COLUMN_URLS_SMALL, currentPhoto.urls.small);
+
+                Uri uri = getContentResolver().insert(
+                        PhotosplashContract.Favorites.CONTENT_URI,
+                        values
+                );
+
+                newStatus = true;
+            }
+            return newStatus;
+        }
+
+
+        /**
+         * <p>Runs on the UI thread after {@link #doInBackground}. The
+         * specified result is the value returned by {@link #doInBackground}.</p>
+         */
+        @Override
+        protected void onPostExecute(Boolean newStatus) {
+            //super.onPostExecute(newStatus);
+            isFavorite = newStatus;
+            updateFavoriteFAB(newStatus);
+        }
+    }
+
+    private void updateFavoriteFAB(Boolean favoriteStatus) {
+        if (favoriteStatus) {
+            fabFavorite.setImageResource(R.drawable.heart_full);
+        } else {
+            fabFavorite.setImageResource(R.drawable.heart);
+        }
     }
 
 
